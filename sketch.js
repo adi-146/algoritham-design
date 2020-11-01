8d804e78de0f977a35ab371ab2a6a391e1f516a7
adi-146 <73242473+adi-146@users.noreply.github.com>
2020-11-01 14:39:08 +0530

    custom function accepts two arguments

--- sketch.js
+++ sketch.js
@@ -1,31 +1,36 @@
 
-var fixedRect,movingRect;
+var fixedRect,movingRect,gameobject1,gameobject2,gameobject3,gameobject4;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1200,400);
 fixedRect= createSprite(600, 400, 50, 80);
 movingRect= createSprite(400, 200, 80, 30);
-fixedRect.shapeColor="red";
-movingRect.shapeColor="red";
+fixedRect.shapeColor="green";
+movingRect.shapeColor="green";
 fixedRect.debug=true;
 movingRect.debug=true;
+gameobject1=createSprite(100,100,50,50);
+gameobject1.shapeColor="green";
+gameobject2=createSprite(300,100,50,50);
+gameobject2.shapeColor="green";
+gameobject3=createSprite(500,100,50,50);
+gameobject3.shapeColor="green";
+gameobject4=createSprite(700,100,50,50);
+gameobject4.shapeColor="green";
 }
 
 function draw() {
   background(255,255,255);  
  movingRect.x=World.mouseX; 
  movingRect.y=World.mouseY;
-
- if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&&
-  fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&movingRect.y-fixedRect.y<movingRect.width/2+fixedRect.width/2&&
-  fixedRect.y-movingRect.y<movingRect.width/2+fixedRect.width/2){
-
-    fixedRect.shapeColor="blue";
-    movingRect.shapeColor="blue";
- }
- else{
+  
+ if( isTouching(movingRect,gameobject1)){
   fixedRect.shapeColor="red";
-  movingRect.shapeColor="red";
+  movingRect.shapeColor="red"; 
  }
+else{
+  fixedRect.shapeColor="green";
+    movingRect.shapeColor="green";
+}
   drawSprites();
 }
